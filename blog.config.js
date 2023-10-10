7ffcf0303605589b4deb59d3983ade6ca73d41ba
TonyChenMou <129352351+TonyChenMou@users.noreply.github.com>
2023-10-10 15:52:33 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -18,8 +18,7 @@ const BLOG = {
   CUSTOM_MENU: process.env.NEXT_PUBLIC_CUSTOM_MENU || false, // 支持Menu 类型，从3.12.0版本起，各主题将逐步支持灵活的二级菜单配置，替代了原来的Page类型，此配置是试验功能、默认关闭。
 
   AUTHOR: process.env.NEXT_PUBLIC_AUTHOR || 'Tony·Chen', // 您的昵称 例如 tangly1024
-  BIO: process.env.NEXT_PUBLIC_BIO || '一个坚持断墨寻径的平凡人罢了
-    (刷新1~3次可更新内容)', // 作者简介
+  BIO: process.env.NEXT_PUBLIC_BIO || '一个坚持断墨寻径的平凡人罢了(刷新1~3次可更新内容)', // 作者简介
   LINK: process.env.NEXT_PUBLIC_LINK || 'https://www.tony-chen.xyz/', // 网站地址
   KEYWORDS: process.env.NEXT_PUBLIC_KEYWORD || 'Notion, 博客', // 网站关键词 英文逗号隔开
 
